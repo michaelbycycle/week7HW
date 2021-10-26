9cc363ebaf4f7ded9e505c9f98ab8eb1f6a28447
michaelbycycle <michaeljwade2@gmail.com>
2021-10-25 20:13:44 -0600

    Add files via upload

---
+++ Database.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace Sept9
+{
+    public class Database
+    {
+        private string getConnectionString()
+        {
+            string connection = ConfigurationManager.ConnectionStrings["ConnectionString"].ToString();
+            return connection;
+        }
+    
+        private SqlConnection OpenDatabase()
+        {
+            string connection = getConnectionString();
+            //variable for database connection
+            SqlConnection myConnection;
+
+            myConnection = new SqlConnection(connection);
+            //A tunnel for data to flow back and forth
+            myConnection.Open();
+            return myConnection;
+        }
+
+        public DataSet getAllAddresses() 
+        {
+            SqlConnection myConnection = OpenDatabase();
+            string query = "spSelectAllAddresses_wade";
+
+            //Data set is like an array - object that holds lots of other stuff
+            DataSet myDataSet = new DataSet();
+            SqlCommand myCommand = new SqlCommand(query);
+            myCommand.Connection = myConnection;
+            myCommand.CommandType = CommandType.StoredProcedure;
+
+            SqlDataAdapter myAdapter = new SqlDataAdapter(myCommand);
+            myAdapter.Fill(myDataSet);
+
+            closeDatabase(myConnection);
+            return myDataSet;
+        }
+
+        private void closeDatabase(SqlConnection myConnection)
+        {
+            myConnection.Close();
+        }
+
+        public void insertNewAddresses(string Address, string City)
+        {
+            SqlConnection myConnection = OpenDatabase();
+            string query = "spInsertNewAddress_wade";
+            SqlParameter[] sqlParameters = new SqlParameter[2];
+            sqlParameters[0] = new SqlParameter("address", Address);
+            sqlParameters[1] = new SqlParameter("city", City);
+
+           
+
+            SqlCommand myCommand = new SqlCommand(query);
+            myCommand.Connection = myConnection;
+            myCommand.CommandType = CommandType.StoredProcedure;
+            myCommand.Parameters.AddRange(sqlParameters);
+
+            myCommand.ExecuteNonQuery();
+            closeDatabase(myConnection);
+        }
+    }
+}
---
+++ spDisplay.aspx.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Sept9
+{
+    public partial class WebForm10 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                showTable();
+            }
+        }
+
+        protected void btnSubmit_Click1(object sender, EventArgs e)
+        {
+            Response.Write("On Button Click");
+
+            //retrieve first name from textbox
+            string Address = txtAddress.Text;
+
+            //retrieve last name from textbox
+            string City = txtCity.Text;
+
+            Database myDatabase = new Database();
+            myDatabase.insertNewAddresses(Address, City);
+
+            //string connection = ConfigurationManager.ConnectionStrings["ConnectionString"].ToString();
+
+            //string query = "spInsertNewAddress_wade";
+            //SqlParameter[] sqlParameters = new SqlParameter[2];
+            //sqlParameters[0] = new SqlParameter("address", Address);
+            //sqlParameters[1] = new SqlParameter("city", City);
+
+            //connection string, server name, username, password
+            //string connection = ConfigurationManager.ConnectionStrings["ConnectionString"].ToString();
+
+            //SqlConnection myConnection;
+
+            //myConnection = new SqlConnection(connection);
+            //A tunnel for data to flow back and forth
+            //myConnection.Open();
+
+            /*SqlCommand myCommand = new SqlCommand(query);
+            myCommand.Connection = myConnection;
+            myCommand.CommandType = CommandType.StoredProcedure;
+            myCommand.Parameters.Add(new SqlParameter("address", Address));
+            myCommand.Parameters.Add(new SqlParameter("city", City));
+            myCommand.ExecuteNonQuery();*/
+
+            //myConnection.Close();
+
+            showTable();
+        }
+
+        private void showTable()
+        {
+            //connection string, server name, username, password
+            //string connection = ConfigurationManager.ConnectionStrings["ConnectionString"].ToString();
+
+            //SqlConnection myConnection;
+
+            //myConnection = new SqlConnection(connection);
+            //A tunnel for data to flow back and forth
+            //myConnection.Open();
+
+            //can test to see if the connection worked
+            // Response.Write(myConnection.State);
+
+
+
+            //to execute a SELECT, we need the following
+            //string query = "spSelectAllAddresses_wade";
+
+            //Data set is like an array - object that holds lots of other stuff
+            //DataSet myDataSet = new DataSet();
+            //go and get me information - use this query i just created
+            //SqlCommand myCommand = new SqlCommand(query);
+            //myCommand.Connection = myConnection;
+            //myCommand.CommandType = CommandType.StoredProcedure;
+
+            //use our command and put into our array
+            //SqlDataAdapter myAdapter = new SqlDataAdapter(myCommand);
+            //myAdapter.Fill(myDataSet);
+
+            //myConnection.Close();
+
+            Database myDatabase = new Database();
+            DataSet myDataSet = myDatabase.getAllAddresses();
+
+            gvUsers.DataSource = myDataSet.Tables[0];
+            gvUsers.DataBind();
+        }
+    }
+}
